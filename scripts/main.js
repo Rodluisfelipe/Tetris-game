14fce927110040d1df67afd0db8c334d0c63bfdb
FelipeSoftlu <pipe95141007@gmail.com>
2022-11-11 21:57:47 -0500

    funcion para eliminar lineas completas

--- scripts/main.js
+++ scripts/main.js
@@ -23,3 +23,28 @@ document.getElementById("restart_game").addEventListener("click", function () {
 
 const canvas = document.getElementById('tetris');
 const context = canvas.getContext('2d');
+
+
+    context.shadowColor = 'black';
+    context.shadowOffsetX = 0.5;
+    context.shadowOffsetY = 0.5;
+
+    context.scale(20, 20);
+
+    function arenaSweep() {
+        let rowCount = 1;
+        outer: for (let y = arena.length - 1; y > 0; --y) {
+            for (let x = 0; x < arena[y].length; ++x) {
+                if (arena[y][x] === 0) {
+                    continue outer;
+                }
+            }
+
+            const row = arena.splice(y, 1)[0].fill(0);
+            arena.unshift(row);
+            ++y;
+
+            player.score += rowCount * 10;
+            rowCount *= 2;
+        }
+    };
