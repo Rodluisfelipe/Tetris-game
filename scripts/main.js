6bd48a0c0dd12f69154a7d412a7df6effe2016cc
FelipeSoftlu <pipe95141007@gmail.com>
2022-11-11 22:15:51 -0500

    Game piece rotation function

--- scripts/main.js
+++ scripts/main.js
@@ -162,6 +162,25 @@ const context = canvas.getContext('2d');
             });
         });
     };
+    function rotate(matrix, dir) {
+        for (let y = 0; y < matrix.length; ++y) {
+            for (let x = 0; x < y; ++x) {
+            [
+                matrix[x][y],
+                matrix[y][x],
+            ] = [
+                matrix[y][x],
+                matrix[x][y],
+            ];
+            }
+        }
+       if (dir > 0) {
+            matrix.forEach(row => row.reverse());
+        } else {
+            matrix.reverse();
+        }
+    }
+
 
     function playerRotate(dir) {
         const pos = player.pos.x;
